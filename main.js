23ae05ab35faf54a2e6dd5ad2d4b273bf4175d57
Antonio Savino <antonio.savino88@gmail.com>
2024-12-29 13:14:07 +0100

    colori navbar aggiustati

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("nav-scrolled");
     if (!home.classList.contains("active-custom")) {
       title.classList.remove("nav-section-custom");
+      home.classList.remove("nav-section-custom");
+      wedding.classList.remove("nav-section-custom");
+      liveMusic.classList.remove("nav-section-custom");
+      teaching.classList.remove("nav-section-custom");
+      contatti.classList.remove("nav-section-custom");
+      home.classList.add("nav-section-custom-2");
+      wedding.classList.add("nav-section-custom-2");
+      liveMusic.classList.add("nav-section-custom-2");
+      teaching.classList.add("nav-section-custom-2");
+      contatti.classList.add("nav-section-custom-2");
     } else {
       title.classList.remove("hidden-visibility");
       title.classList.add("visible-visibility");
@@ -20,6 +30,16 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("nav-scrolled");
     if (!home.classList.contains("active-custom")) {
       title.classList.add("nav-section-custom");
+      home.classList.add("nav-section-custom");
+      wedding.classList.add("nav-section-custom");
+      liveMusic.classList.add("nav-section-custom");
+      teaching.classList.add("nav-section-custom");
+      contatti.classList.add("nav-section-custom");
+      home.classList.remove("nav-section-custom-2");
+      wedding.classList.remove("nav-section-custom-2");
+      liveMusic.classList.remove("nav-section-custom-2");
+      teaching.classList.remove("nav-section-custom-2");
+      contatti.classList.remove("nav-section-custom-2");
     } else {
       title.classList.add("hidden-visibility");
       title.classList.remove("visible-visibility");
