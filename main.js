ab83aaeff9be1e110c2075a2a65a84906d9e543f
Antonio Savino <antonio.savino88@gmail.com>
2024-12-29 12:29:48 +0100

    migliorata responsività estetica generale e colori navbar nelle sezioni

--- main.js
+++ main.js
@@ -12,11 +12,6 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("nav-scrolled");
     if (!home.classList.contains("active-custom")) {
       title.classList.remove("nav-section-custom");
-      home.classList.remove("nav-section-custom");
-      wedding.classList.remove("nav-section-custom");
-      liveMusic.classList.remove("nav-section-custom");
-      teaching.classList.remove("nav-section-custom");
-      contatti.classList.remove("nav-section-custom");
     } else {
       title.classList.remove("hidden-visibility");
       title.classList.add("visible-visibility");
@@ -25,11 +20,6 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("nav-scrolled");
     if (!home.classList.contains("active-custom")) {
       title.classList.add("nav-section-custom");
-      home.classList.add("nav-section-custom");
-      wedding.classList.add("nav-section-custom");
-      liveMusic.classList.add("nav-section-custom");
-      teaching.classList.add("nav-section-custom");
-      contatti.classList.add("nav-section-custom");
     } else {
       title.classList.add("hidden-visibility");
       title.classList.remove("visible-visibility");
